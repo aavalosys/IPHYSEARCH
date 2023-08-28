d2b3b7f23319740d214968b74b61659054bdacd8
NELSON ALFREDO AVALOS ACEVEDO <AVALOS.NELSON@claro.com.gt>
2023-08-28 11:51:02 -0600

    CONFIG INICIALES

---
+++ iphysearchapp/iphysearchapp/databases.py
@@ -0,0 +1,16 @@
+from .var_env import ENGINE, USER, PASSWORD, HOST, PORT, USERBK, PASSWORDBK, HOSTBK
+
+DATABASES={
+            'default': {
+            'ENGINE': ENGINE,
+            'NAME': 'fy2023w30',
+            'USER': USER,
+            'PASSWORD': PASSWORD,
+            'HOST': HOST,
+            'PORT': PORT,
+            }
+        }
+
+
+
+
---
+++ iphysearchapp/iphysearchapp/var_env.py
@@ -0,0 +1,12 @@
+USER = 'navalos'
+PASSWORD = '44&7Ai0YHbD#'
+ENGINE = 'django.db.backends.mysql'
+HOST = '10.10.26.4'
+PORT = '3306'
+
+USERBK = 'nelson.avalos'
+PASSWORDBK = 'Navalos20230720#'
+HOSTBK = '10.10.26.5'
+DBBK = 'temp_db'
+
+DBSINICIAL ='f%'
